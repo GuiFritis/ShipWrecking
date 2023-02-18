99faa6d6c8c7e999ef2ab55879970103773e8d46
GuiFritis <Guilhermecfritis@gmail.com>
2023-02-18 12:42:02 -0300

    add SO helpers

---
+++ Assets/Scripts/Utils/SO_Helpers/SOFloat.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu]
+public class SOFloat : ScriptableObject
+{
+    public Action<float> OnValueChanged;
+
+    public float Value
+    {
+        get{return _value;}
+        set
+        {
+            _value = value;
+            OnValueChanged?.Invoke(_value);
+        }
+    }
+
+    private float _value;
+}

a86fdd6ce11a86c834218f8e29bfb37c7cbf5036
GuiFritis <Guilhermecfritis@gmail.com>
2023-02-20 09:31:25 -0300

    add scene ambience music and enchance shotVfx

--- Assets/Scripts/Helpers/DestroyHelper.cs
+++ Assets/Scripts/Helpers/DestroyHelper.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class DestroyHelper : MonoBehaviour
 {
     [Tooltip("If destroy is checked, the game object will be destroyed, else it will be inactivated")]
     public bool destroy = false;
+    public float timeToDestroy = 2f;
 
-    public void DeleteMe()
+    void Start()
     {
+        StartCoroutine(DeleteMe());
+    }
+
+    private IEnumerator DeleteMe()
+    {
+        yield return new WaitForSeconds(timeToDestroy);
         if(destroy)
         {
             Destroy(gameObject);
